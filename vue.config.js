cd2221ae2a388ebb4cdf0032acea3e798a40eeb7
99611400 <657793418@qq.com>
2020-03-08 20:39:55 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports ={
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'store': '@/store',
+        'network': '@/network',
+        'views': '@/views',
+
+      }
+    }
+  }
+}
